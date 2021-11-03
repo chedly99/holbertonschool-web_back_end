2f564dbaba3a8f7f309b54a2f4cf15bd2cc972a4
chedly99 <mohamed.chedliiy@gmail.com>
2021-11-03 15:27:13 +0100

    fix

--- 0x03-caching/1-fifo_cache.py
+++ 0x03-caching/1-fifo_cache.py
@@ -1,21 +1,20 @@
 #!/usr/bin/python3
-""" Fifo_cashe py """
+""" 1.FIFO caching py """
+
 
 BaseCaching = __import__('base_caching').BaseCaching
 
 
-class BasicCache(BaseCaching):
-    """[summary]
-    FIFO cache class
-    """
+class FIFOCache(BaseCaching):
+    """FIFO Cache class"""
+
     def __init__(self):
+        """ Class """
         super().__init__()
 
     def put(self, key, item):
-        """[summary]
-        assign to the dictionary self.cache_data,
-        the item value for the key key
-        """
+        """ assign to the dictionary self.cache_data
+        the item value for the key key."""
 
         if key not in self.cache_data.keys():
             if len(self.cache_data) == BaseCaching.MAX_ITEMS:
@@ -27,7 +26,7 @@ class BasicCache(BaseCaching):
         self.cache_data[key] = item
 
     def get(self, key):
-        """return the value in self."""
+        """return the value in self.cache_data linked to key """
         if key is None or key not in self.cache_data.keys():
             return None
 
