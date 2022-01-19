8bbfa7c4881dc757704020240860606938e45f47
chedly99 <mohamed.chedliiy@gmail.com>
2022-01-19 21:24:05 +0100

    task100

---
+++ 0x11-ES6_data_manipulation/100-weak.js
@@ -0,0 +1,7 @@
+export const weakMap = new WeakMap();
+
+export function queryAPI(endpoint) {
+  const count = weakMap.get(endpoint) || 0;
+  if (count >= 4) throw new Error('Endpoint load is high');
+  weakMap.set(endpoint, count + 1);
+}
