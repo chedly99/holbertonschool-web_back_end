b8eed0854809d6e8c880c920f65b666507f5ff69
chedly99 <mohamed.chedliiy@gmail.com>
2022-01-19 21:23:10 +0100

    task10

---
+++ 0x11-ES6_data_manipulation/10-update_uniq_items.js
@@ -0,0 +1,9 @@
+export default function updateUniqueItems(groceriesMap) {
+  if (!(groceriesMap instanceof Map)) throw new Error('Cannot process');
+
+  groceriesMap.forEach((value, key) => {
+    if (value === 1) groceriesMap.set(key, 100);
+  });
+
+  return groceriesMap;
+}
